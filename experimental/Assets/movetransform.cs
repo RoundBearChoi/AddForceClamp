81b51838fa86c157b5fd53574cdccdc8e5603b83
RoundBearChoi <roundbeargames@gmail.com>
2020-04-17 14:03:51 +0900

    transform test

---
+++ experimental/Assets/movetransform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movetransform : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody r;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            r.MovePosition(this.transform.position + this.transform.forward * speed);
+            //this.transform.position += this.transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            r.MovePosition(this.transform.position - this.transform.forward * speed);
+            //this.transform.position -= this.transform.forward * speed;
+        }
+    }
+}
